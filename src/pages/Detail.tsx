d35271cf08b011967c3500c89fab5017a96aefd8
Ericano Rhee <ericano.rhee@gmail.com>
2023-12-03 11:50:36 +0900

    231203_ETA-Client 11:50 티맵 작업 중

--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect} from "react";
 import DetailHeader from "../components/DetailHeader";
 import DetailMap from "../components/DetailMap";
 import axios from "axios";
@@ -9,13 +9,35 @@ async function select(calendar_id: String, event_id: String) {
     });
 }
 
+function initTmap() {
+    let map = new Tmapv2.Map("map_div",
+        {
+            center: new Tmapv2.LatLng(37.566481622437934, 126.98502302169841), // 지도 초기 좌표
+            width: "890px",
+            height: "400px",
+            zoom: 15
+        });
+}
+
 function Detail() {
-  return (
-    <>
-      <DetailHeader />
-      <DetailMap />
-    </>
-  );
+
+
+    useEffect(() => {
+        const script = document.createElement("script");
+        script.src = `https://apis.openapi.sk.com/tmap/jsv2?version=1&appKey=${process.env.REACT_APP_TMAP_API_KEY}`;
+        script.async = true;
+        // script.addEventListener("load", initTmap);
+        document.body.appendChild(script);
+    }, []);
+
+    return (
+        <>
+            <DetailHeader/>
+            <div id={"map_div"}/>
+            <DetailMap/>
+            <button onClick={initTmap} />
+        </>
+    );
 }
 
 export default Detail;
