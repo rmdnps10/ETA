cb566030e158ef07ff5b47f0bbeaacacf12ff780
inyoung <rmdnps10@gmail.com>
2023-12-04 15:50:14 +0900

    Package: 날짜 관련 라이브러리 설치

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,25 +1,15 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import MainHeader from "../components/MainHeader";
 import PlanCardList from "../components/PlanCardList";
-import {instance} from "../api/axios";
-import {gapi} from "gapi-script";
+import { instance } from "../api/axios";
+import { gapi } from "gapi-script";
 
 function Main() {
-    useEffect(() => {
-        const fetchData = async () => {
-            const res = await instance.get("/posts");
-            console.log(res.data[2].title);
-        };
-        fetchData();
-
-
-    }, []);
-    return (
-        <>
-            <MainHeader/>
-            <PlanCardList/>
-        </>
-    );
+  return (
+    <>
+      <MainHeader />
+      <PlanCardList />
+    </>
+  );
 }
-
 export default Main;
