3fa48e292dc842bbdb7bffa8609a2911adab80cd
Ericano Rhee <ericano.rhee@gmail.com>
2023-12-04 16:36:03 +0900

    231204_ETA-Client 16:35 Detail 데이터 받아옵니다. todo : detail에 query로 calendar_id, event_id를 받아와서 넘겨주세요

--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, {useEffect} from "react";
 import DetailHeader from "../components/DetailHeader";
 import DetailMap from "../components/DetailMap";
 import axios from "axios";
@@ -7,77 +7,197 @@ import PrepareItem from "components/getDirections/PrepareItem";
 import DestinationItem from "components/getDirections/DestinationItem";
 import SubwayItem from "components/getDirections/SubwayItem";
 import WalkItem from "components/getDirections/WalkItem";
+import {gapi} from "gapi-script";
+import sample_routes from "../test/sample_routes_req.json";
 
 // const {Tmapv2} = window;
 
+function insert(calendar_id: String, event_id: String, is_enabled: boolean, address: String, lat: number, lng: number, routes: String) {
+    axios.post("http://localhost:8000/insert", {
+        event_id: event_id,
+        calendar_id: calendar_id,
+        is_enabled: is_enabled,
+        address: address,
+        lat: lat,
+        lng: lng,
+        routes: routes
+    })
+        .then((res) => {
+            console.log(res);
+        })
+        .catch((e) => {
+            console.log(e);
+        })
+}
+
 async function select(calendar_id: String, event_id: String) {
-  return axios
-    .get(
-      `http://localhost:8000/list?calendar_id=${calendar_id}&event_id=${event_id}`,
-      {}
-    )
-    .then((res) => {
-      return res;
-    });
+    return axios
+        .get(
+            `http://localhost:8000/list?calendar_id=${calendar_id}&event_id=${event_id}`,
+            {}
+        )
+        .then((res) => {
+            return res;
+        });
 }
 
 function initTmap() {
-  let map = new Tmapv3.Map("map_div", {
-    center: new Tmapv3.LatLng(37.566481622437934, 126.98502302169841), // 지도 초기 좌표
-    width: "890px",
-    height: "400px",
-    zoom: 15,
-  });
+    let map = new Tmapv3.Map("map_div", {
+        center: new Tmapv3.LatLng(37.566481622437934, 126.98502302169841), // 지도 초기 좌표
+        width: "890px",
+        height: "400px",
+        zoom: 15,
+    });
 }
 
 function Detail() {
-  /** -> todo map 구현. 횟수 차감 위험으로 일단 주석처리
-  useEffect(() => {
-    if (
-        document.querySelector(
-            `script[src="https://apis.openapi.sk.com/tmap/jsv2?version=1&appKey=${process.env.REACT_APP_TMAP_API_KEY}"]`
-        )
-    ) return;
-    const script = document.createElement("script");
-    script.src = `https://apis.openapi.sk.com/tmap/jsv2?version=1&appKey=${process.env.REACT_APP_TMAP_API_KEY}`;
-    script.async = true;
-    // script.addEventListener("load", initTmap);
-    document.body.appendChild(script);
-    const script2 = document.createElement("script");
-    script2.src = `https://toptmaptile2.tmap.co.kr/scriptSDKV3/tmapjs3.min.js?version=20230906`;
-    script2.async = true;
-    script.addEventListener("load", initTmap);
-    document.body.appendChild(script2);
-  }, []);
-      */
+    useEffect(() => {
+        if (
+            document.querySelector(
+                `script[src="https://toptmaptile2.tmap.co.kr/scriptSDKV3/tmapjs3.min.js?version=20230906"]`
+            )
+        ) return;
+        // const script = document.createElement("script");
+        // script.src = `https://apis.openapi.sk.com/tmap/jsv2?version=1&appKey=${process.env.REACT_APP_TMAP_API_KEY}`;
+        // script.async = true;
+        // script.addEventListener("load", initTmap);
+        // document.body.appendChild(script);
+        const script2 = document.createElement("script");
+        script2.src = `https://toptmaptile2.tmap.co.kr/scriptSDKV3/tmapjs3.min.js?version=20230906`;
+        script2.async = true;
+        script2.addEventListener("load", initTmap);
+        document.body.appendChild(script2);
 
-  return (
-    <>
-      <DetailHeader />
-      <div id={"map_div"} />
-      {/*<TmapSection />*/}
-      <DetailInfo>
-        <DetailBasicInfo>
-          <PlanTime>
-            오전 <span>10:30</span>
-          </PlanTime>
-          <PlanSpace>
-            서강대학교 김대건관, 대한민국 서울특별시 마포구 신수동 1-6
-          </PlanSpace>
-        </DetailBasicInfo>
+    }, []);
 
-        <DetailGetDirections>
-          <PrepareItem time={"오전 9:11"} />
-          <SubwayItem />
-          <WalkItem />
-          <DestinationItem
-            destination={"서강대학교 김대건관"}
-            time={"오전 10:30"}
-          />
-        </DetailGetDirections>
-      </DetailInfo>
-    </>
-  );
+    const initGAPI = async () => {
+        try {
+            await new Promise((resolve) => gapi.load("client:auth2", resolve));
+
+            gapi.auth2.init({
+                client_id: process.env.REACT_APP_GOOGLE_CLOUD_CLIENT_ID,
+            });
+
+            await gapi.client.load(
+                "https://content.googleapis.com/discovery/v1/apis/calendar/v3/rest"
+            );
+
+            const event_response = await gapi.client.calendar.events.get({
+                calendarId: "ericano.rhee@gmail.com", // query에서 받아온 calendar_id 넣어주기
+                eventId: "68pjedr3cdi38b9i6gqm8b9k6oqm8b9o6ss64b9j6cqjae9mcgp36pb468" // query에서 받아온 event_id 받아주기
+            });
+            const calendar_response = await gapi.client.calendar.calendarList.get({
+                calendarId: "ericano.rhee@gmail.com"
+            });
+
+            console.log(event_response.result);
+            console.log(calendar_response.result);
+            console.log(event_response)
+
+            const event = {
+                is_enabled: true,
+                event_id: event_response.result.id,
+                calendar_id: calendar_response.result.id,
+                title: event_response.result.summary,
+                description: event_response.result.description,
+                startDate: event_response.result.start.dateTime,
+                startTimeZone: event_response.result.start.timeZone,
+                endDate: event_response.result.end.dateTime,
+                endTimeZone: event_response.result.end.dateTime,
+                color: calendar_response.result.backgroundColor,
+                eventLocation: event_response.result.location,
+                lat: 0.0,
+                lng: 0.0,
+                routes: ""
+            };
+
+            const data = await select(event.calendar_id, event.event_id);
+            if (data.data.length == 0) { // not on database
+                let geoUrl = "https://maps.googleapis.com/maps/api/geocode/json?" +
+                    "address=" +
+                    encodeURI(event.eventLocation) +
+                    "&region=ko&" +
+                    `key=${process.env.REACT_APP_GOOGLE_CLOUD_CALENDAR_API_KEY}`
+
+                const cor_response = await fetch(geoUrl, {
+                    method: "GET",
+                    headers: {
+                        Accept: "application/json",
+                    },
+                });
+
+                const cor = await cor_response.json();
+
+                if (cor.results.length !== 0) {
+                    let coordinate = cor.results[0].geometry.location;
+                    event.lat = coordinate.lat;
+                    event.lng = coordinate.lng;
+                }
+
+                /** 아래 내용은 API 제한 때문에 주석처리하고 임의로 raw 데이터로 대신함. 실제 테스트 시 대신 켜세요
+                 let routes_response = await axios.post("https://apis.openapi.sk.com/transit/routes", {
+                 "startX": localStorage.getItem("home_address_lng"),
+                 "startY": localStorage.getItem("home_address_lat"),
+                 "endX": event.lng,
+                 "endY": event.lat,
+                 "count": 1,
+                 "lang": 0,
+                 "format": "json",
+                 }, {
+                 headers: {
+                 Accept: "application/json",
+                 appKey: process.env.REACT_APP_TMAP_API_KEY,
+                 'content-type': "application/json"
+                 }
+                 });
+
+                 let routes_result = JSON.stringify(routes_response.data.metaData);
+                 */
+
+                let routes_result = JSON.stringify(sample_routes)
+                insert(event.calendar_id, event.event_id, event.is_enabled, event.eventLocation, event.lat, event.lng, routes_result);
+            } else {
+                event.is_enabled = data.data[0].is_enabled;
+                event.lat = data.data[0].lat;
+                event.lng = data.data[0].lng;
+                event.routes = data.data[0].routes;
+            }
+
+            console.log(event);
+        } catch (err) {
+            console.error("Error loading GAPI or fetching calendar list", err);
+        }
+    };
+
+    initGAPI();
+
+    return (
+        <>
+            <DetailHeader/>
+            <div id={"map_div"}/>
+            {/*<TmapSection />*/}
+            <DetailInfo>
+                <DetailBasicInfo>
+                    <PlanTime>
+                        오전 <span>10:30</span>
+                    </PlanTime>
+                    <PlanSpace>
+                        서강대학교 김대건관, 대한민국 서울특별시 마포구 신수동 1-6
+                    </PlanSpace>
+                </DetailBasicInfo>
+
+                <DetailGetDirections>
+                    <PrepareItem time={"오전 9:11"}/>
+                    <SubwayItem/>
+                    <WalkItem/>
+                    <DestinationItem
+                        destination={"서강대학교 김대건관"}
+                        time={"오전 10:30"}
+                    />
+                </DetailGetDirections>
+            </DetailInfo>
+        </>
+    );
 }
 
 const TmapSection = styled.div`
@@ -115,6 +235,7 @@ const PlanTime = styled.div`
   font-style: normal;
   font-weight: 700;
   line-height: 24px; /* 100% */
+
   span {
     color: #32283e;
     font-size: 36px;
